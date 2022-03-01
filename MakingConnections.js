2627766f806705803afbc2db3b686db290b89654
Venus Quan <94498212+vxq5000@users.noreply.github.com>
2022-03-01 14:41:53 -0800

    Add files via upload
    Making Connections Assignment

---
+++ MakingConnections.js
@@ -0,0 +1,12 @@
+var change= document.querySelector("#username");
+var deleteuser=document.querySelector("#userpic")
+var deleteuser2=document.querySelector("#userpic2")
+function nameChange(){
+    change.innerText="Venus Quan";
+}
+function removeUser(){
+    deleteuser.remove();
+}
+function removeUser2(){
+    deleteuser2.remove();
+}
